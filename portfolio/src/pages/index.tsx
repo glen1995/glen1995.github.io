63a069e0ac488dec405f5ce9ae99d2eab77108c4
Glen Thomas <glen.thomas@visualping.io>
2024-07-21 19:22:03 -0700

    storing snapshot for app directory

---
+++ portfolio/src/pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+export default function HomePage() {
+    console.log("This is the homepage.");
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the main content of the homepage.</p>
+        </div>
+    );
+}
