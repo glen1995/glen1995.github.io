bf41cf2c3523523dda2edcea5d83c30b7ddf64a9
Glen Thomas <glen.thomas@visualping.io>
2024-08-03 12:26:38 -0700

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
     basePath: isProd ? "" : "",
     output: "export",
+    trailingSlash: true,
     reactStrictMode: true,
     assetPrefix: isProd ? "/" : "",
     images: {
