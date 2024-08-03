c14903184d71719c629a7ca87ff0c870b1fc29b2
Glen Thomas <glen.thomas@visualping.io>
2024-08-03 12:13:53 -0700

    actions for deploying github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+    basePath: isProd ? "" : "",
+    output: "export",
+    reactStrictMode: true,
+    assetPrefix: isProd ? "/" : "",
+    images: {
+        unoptimized: true,
+    }
+};
 
 export default nextConfig;
